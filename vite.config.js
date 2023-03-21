331ce5e0c45e5a6f959efad376e0037bfb6221b9
Sebastian Post <sebastianpost@protonmail.com>
2023-03-21 13:54:11 +0100

    sfsd

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,7 @@
 import { defineConfig } from 'vite';
 
-/*
-Om ditt repo har adressen https://github.com/Medieinstitutet/fed22d-js-grundkurs-jenni-wumpus,
-då ska "base" här nedan vara "/fed22d-js-grundkurs-jenni-wumpus/"
- */
-
 export default defineConfig({
-  base: 'CV-Vite', // TODO - ändra till ditt repo-namn
+  base: '/CV-LIA', 
   define: {
     'process.env': {},
   },
